ceda697a468057590178d7358685c351e345c954
amandluz <amandluz@live.com>
2024-09-06 18:40:16 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,19 +3,19 @@ let dados = [
         titulo : "Cidade de Deus",
         descricao : "Os caminhos de duas crianças divergem nas favelas do Rio, enquanto um se esforça para se tornar um fotógrafo e o outro um chefe do tráfico.",
         link : "https://www.imdb.com/title/tt0317248/?ref_=ls_t_1",
-        tags: "traficantes"
+        tags: "traficantes zé pequeno ze dadinho dado"
     },
     {
         titulo : "O Auto da Compadecida",
         descricao : "No Brasil, dois nordestinos pobres e espertos vivem de enganar pessoas para sobreviver. Quando conhecem uma moça rica, eles têm esperança de se darem bem, mas seus planos são interrompidos pela chegada de um bandido forasteiro.",
         link : "https://www.imdb.com/title/tt0271383/?ref_=ls_t_2",
-        tags: "zé grilo ze grilo chicó chico"
+        tags: "zé grilo ze grilo chicó chico alto"
     },
     {
         titulo : "Tropa de Elite",
         descricao : "Em 1997, o capitão Nascimento tem que encontrar seu substituto enquanto luta contra os narcotraficantes e criminosos antes da chegada do Papa ao Rio de Janeiro, Brasil.",
         link : "https://www.imdb.com/title/tt0861739/?ref_=ls_t_3",
-        tags: "wagner moura"
+        tags: "wagner moura capitão capitao Nascimento"
     },
     {
         titulo : "Central do Brasil",
